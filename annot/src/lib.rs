b0716680b1c40cd6aed1f16aa6af2b63aee7462c
Xiuxuan Wang <36021591+wxxedu@users.noreply.github.com>
2023-05-23 14:25:00 +0800

    feat(proc): add proc macro on the rust side

--- annot/src/lib.rs
+++ annot/src/lib.rs
@@ -1,14 +1,46 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use proc_macro2::TokenStream;
+use syn::{parse_macro_input, Item, ItemEnum, ItemFn, ItemStruct};
+
+extern crate proc_macro;
+
+#[proc_macro_attribute]
+pub fn flusty(
+    _attr: proc_macro::TokenStream,
+    item: proc_macro::TokenStream,
+) -> proc_macro::TokenStream {
+    let item = parse_macro_input!(item as Item);
+    handle_item(&item).into()
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+fn handle_item(item: &Item) -> TokenStream {
+    match item {
+        Item::Fn(f) => handle_item_fn(f),
+        Item::Struct(s) => handle_item_struct(s),
+        Item::Enum(e) => handle_item_enum(e),
+        _ => panic!("flusty can only be used on functions"),
+    }
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+fn handle_item_fn(f: &ItemFn) -> TokenStream {
+    let sig = &f.sig;
+    let body = &f.block;
+    quote::quote! {
+        pub extern "C" #sig {
+            #body
+        }
+    }
+}
+
+fn handle_item_struct(s: &ItemStruct) -> TokenStream {
+    quote::quote! {
+        #[repr(C)]
+        #s
+    }
+}
+
+fn handle_item_enum(e: &ItemEnum) -> TokenStream {
+    quote::quote! {
+        #[repr(C)]
+        #e
     }
 }
